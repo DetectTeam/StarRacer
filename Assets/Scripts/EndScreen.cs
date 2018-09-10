d3cb2a098b64bd70d7b6229a2c498880e20d58b9
Eoghan <gallageo@tcd.ie>
2018-09-10 14:14:08 +0100

    Added Popup Dialog Box for end of each level

--- Assets/Scripts/EndScreen.cs
+++ Assets/Scripts/EndScreen.cs
@@ -6,6 +6,10 @@ using UnityEngine;
 
 public class EndScreen : MonoBehaviour 
 {
+	
+	[SerializeField] private float speed = 6.0f;
+	[SerializeField] private GameObject endScreenBackground;
+
 
 	// [SerializeField] private PostProcessingProfile blurProfile;
 	// [SerializeField] private PostProcessingProfile defaultProfile;
@@ -20,5 +24,26 @@ public class EndScreen : MonoBehaviour
 	// 		cameraPostProcess.profile = ( state ) ? blurProfile : defaultProfile;
 	// 	}
 	// }
+
+	public void ActivateEndBackground()
+	{
+		StartCoroutine( "IEActivateEndBackground" );
+	}
+
+	IEnumerator IEActivateEndBackground()
+	{
+			Debug.Log( "END SCREEN ON ENABLE CALLED" );
+			endScreenBackground.SetActive( true );
+			iTween.ScaleTo ( endScreenBackground, iTween.Hash ("scale", new Vector3 (1.0f,1.0f,1.0f), "speed", speed, "easetype", "linear"));
+
+			yield return new WaitForSeconds( 0.3f );
+
+			//Punch animation when correct star is encountered
+		    iTween.PunchScale( endScreenBackground, iTween.Hash( "x",+.3, "y",+.3, "time",1.0f));
+
+
+
+	}
+
 	
 }
