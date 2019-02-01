ea6d2531ae2e16990ca2905849bd6f6abee5ec59
Eoghan <eoghan.gallagher@gmail.com>
2019-02-01 13:55:26 +0000

    StarRacer Update

--- Assets/Scripts/FileUploadHandler.cs
+++ Assets/Scripts/FileUploadHandler.cs
@@ -17,7 +17,7 @@ public class FileUploadHandler : MonoBehaviour
 
 	private string jsonString;
 
-	//private static readonly string PutSessionURL = "https://arcane-scrubland-18936.herokuapp.com/api/session";
+	//private static readonly string PutSessionURL = "https://murmuring-fortress-76588.herokuapp.com/api/starracer/session";
 	private static readonly string PutSessionURL = "http://localhost:5000/api/starracer/session";
 
 	private void Awake()
